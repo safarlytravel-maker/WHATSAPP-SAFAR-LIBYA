5fae2094ebb6671b91eabe16ca21627fef44d876
safarlytravel-maker <safarlytravel@gmail.com>
2026-03-05 00:50:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,17 +18,35 @@ app.get("/webhook", (req, res) => {
   }
 });
 
-app.post("/webhook", (req, res) => {
-  console.log("Incoming message:", JSON.stringify(req.body, null, 2));
+app.post("/webhook", async (req, res) => {
+
+  const body = req.body;
+
+  if (body.entry) {
+
+    const message = body.entry[0].changes[0].value.messages?.[0];
+
+    if (message) {
+
+      const from = message.from;
+
+      await fetch("https://graph.facebook.com/v18.0/994643217068788/messages", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "BearerEAAMzNHWBHXMBQZCI1TUxbLnFWS3IdXvIBPfBj5Ekorkx5laVD9DAblUETtNx7Shf2W9ZBY8f3bW1ElDV3d4zqZBbiOk40ZBG2yEIyAzpjkhZAC67r3FVxdE8wBRSB5j6tQ7YkYDtekQsoctwFwpvmpKoRqZAYsw8O8BZBt4HZA46nnS2wzGlVmLZArOk4VwSnHZCtSWNSxbmZBQ7RRZCPJUdNLgVZAjB0WJ7sHfRLobkNLNhfeHL3NyXC0hJVkqsMYj9dfb5RuVgTr0kgQFbVVFscMlLqdgeyXuN1Wk9Qn6EhNAZDZD"
+        },
+        body: JSON.stringify({
+          messaging_product: "whatsapp",
+          to: from,
+          text: {
+            body: "مرحبا بك في Safar Libya ✈️\n\n1️⃣ حجز تذاكر\n2️⃣ فنادق\n3️⃣ عروض سياحية"
+          }
+        })
+      });
+
+    }
+  }
+
   res.sendStatus(200);
-});
-
-app.get("/", (req, res) => {
-  res.send("Safar Libya WhatsApp Bot Running");
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
 });
