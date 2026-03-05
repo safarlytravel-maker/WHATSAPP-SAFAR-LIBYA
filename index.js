927e3eb02bd5c66085da6804f899cf96c581f702
safarlytravel-maker <safarlytravel@gmail.com>
2026-03-05 07:40:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post("/webhook", async (req, res) => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": Bearer ${ACCESS_TOKEN}
+          "Authorization": `Bearer ${ACCESS_TOKEN}`
         },
         body: JSON.stringify({
           messaging_product: "whatsapp",
