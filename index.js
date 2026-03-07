87609dbf95ea64d37c559df82a0a49e7a1f5317c
safarlytravel-maker <safarlytravel@gmail.com>
2026-03-07 06:49:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -43,12 +43,12 @@ app.post("/webhook", async (req, res) => {
     if (text === "hi") {
 
 await fetch(
-https://graph.facebook.com/v18.0/${PHONE_NUMBER_ID}/messages,
+`https://graph.facebook.com/v18.0/${PHONE_NUMBER_ID}/messages`,
 {
 method: "POST",
 headers: {
 "Content-Type": "application/json",
-Authorization: Bearer ${ACCESS_TOKEN}
+Authorization: `Bearer ${ACCESS_TOKEN}`
 },
 body: JSON.stringify({
 messaging_product: "whatsapp",
