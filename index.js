1085842b2b1f1816bb19b019bb964900ecf86ad3
safarlytravel-maker <safarlytravel@gmail.com>
2026-03-07 04:17:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ app.post("/webhook", async (req, res) => {
 
       if (reply) {
         await fetch(
-          https://graph.facebook.com/v18.0/${PHONE_NUMBER_ID}/messages,
+          `https://graph.facebook.com/v18.0/${PHONE_NUMBER_ID}/messages`,
           {
             method: "POST",
             headers: {
